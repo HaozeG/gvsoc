950beb86b87aaf3c5315f480fc8e78f9a8f05839
Haoze <haozegao0602@gmail.com>
2025-03-03 17:06:29 +0100

    Refactor HBM address handling and improve preload parameter clarity in matmul example

--- examples/SoftHier/software/matmul/main.c
+++ examples/SoftHier/software/matmul/main.c
@@ -11,24 +11,7 @@ int main(){
     flex_barrier_xy_init();
     flex_global_barrier_xy();
 
-    // Read the value preloaded into HBM
-    uint32_t in_token_addr = (uint32_t)(hbm_addr(0));
-    // uint32_t n_token_addr = ((uint32_t *)(hbm_addr(4)))[0];
-    // uint32_t dim_addr = ((uint32_t *)(hbm_addr(8)))[0];
-    // uint32_t inter_dim_addr = ((uint32_t *)(hbm_addr(12)))[0];
-    // uint32_t n_routed_experts_addr = ((uint32_t *)(hbm_addr(16)))[0];
-    // uint32_t n_shared_experts_addr = ((uint32_t *)(hbm_addr(20)))[0];
-    // uint32_t n_activated_experts_addr = ((uint32_t *)(hbm_addr(24)))[0];
-    uint32_t gate_weights_addr = (uint32_t)(hbm_addr(4));
-    uint32_t expert_w1_weights_addr = (uint32_t)(hbm_addr(8));
-    uint32_t expert_w1_bias_addr = (uint32_t )(hbm_addr(12));
-    uint32_t expert_w2_weights_addr = (uint32_t)(hbm_addr(16));
-    uint32_t expert_w2_bias_addr = (uint32_t)(hbm_addr(20));
-    uint32_t expert_w3_weights_addr = (uint32_t)(hbm_addr(24));
-    uint32_t expert_w3_bias_addr = (uint32_t)(hbm_addr(28));
-    uint32_t actual_out_addr = (uint32_t)hbm_addr(32);
-    uint32_t golden_out_addr = (uint32_t)hbm_addr(36);
-
+    // Parameters below follows the configuration of MoE model used in preload
     uint16_t n_token = 1;
     uint16_t dim = 1024;
     uint16_t inter_dim = 512;
@@ -36,22 +19,81 @@ int main(){
     uint16_t n_shared_experts = 1;
     uint16_t n_activated_experts = 4;
     
+    /**
+     * Shape of matrix:
+     * in_token, actual_out, golden_out: [n_token, dim]
+     * 
+     * Gate
+     *   weight: [dim, n_routed_experts]
+     * Expert
+     *   w1
+     *     weight: [dim, inter_dim]
+     *     bias: [1, inter_dim]
+     *   w2
+     *     weight: [inter_dim, dim]
+     *     bias: [1, dim]
+     *   w3
+     *     weight: [dim, inter_dim]
+     *     bias: [1, inter_dim]
+     */
+    // Read the value preloaded into HBM
+    uint32_t in_token_offset =            0;
+    uint32_t gate_weights_offset =        in_token_offset + n_token * dim * 2;
+    uint32_t expert_w1_weights_offset =   gate_weights_offset + dim * n_routed_experts * 2;
+    uint32_t expert_w1_bias_offset =      expert_w1_weights_offset + dim * inter_dim * 2;
+    uint32_t expert_w2_weights_offset =   expert_w1_bias_offset + inter_dim * 2;
+    uint32_t expert_w2_bias_offset =      expert_w2_weights_offset + inter_dim * dim * 2;
+    uint32_t expert_w3_weights_offset =   expert_w2_bias_offset + dim * 2;
+    uint32_t expert_w3_bias_offset =      expert_w3_weights_offset + dim * inter_dim * 2;
+    uint32_t actual_out_offset =          expert_w3_bias_offset + inter_dim * 2;
+    uint32_t golden_out_offset =          actual_out_offset + n_token * dim * 2;
+
     if (flex_is_first_core() && (flex_get_cluster_id()==0))
     {
         printf("[Check Preload] Addresses\n");
-        printf("in_token: 0x%x\n", in_token_addr);
-        printf("golden: 0x%x\n", golden_out_addr);
+        printf("in_token: 0x%x\n", hbm_addr(in_token_offset));
+        printf("gate_weight: 0x%x\n", hbm_addr(gate_weights_offset));
+        printf("actual: 0x%x\n", hbm_addr(actual_out_offset));
+        printf("golden: 0x%x\n", hbm_addr(golden_out_offset));
         printf("n_token: %x\n", n_token);
-        printf("dim: %x\n", dim);
-        printf("inter_dim: %x\n", inter_dim);
-        printf("n_routed_experts: %x\n",n_routed_experts);
+        printf("dim: %d\n", dim);
+        printf("inter_dim: %d\n", inter_dim);
+        printf("n_routed_experts: %d\n",n_routed_experts);
     }
     
     // First element of each matrix
     if (flex_is_first_core() && (flex_get_cluster_id()==0))
     {
-        printf("[Check Preload] Data\n");
-        printf("0x%04x\n", ((uint16_t *)(hbm_addr(in_token_addr)))[0]);
+        // printf("[Check Preload] Data\n");
+        printf("[Check Preload] with first 8 elements of each row of the input\n");
+        printf("in_token:\n");
+        for (int i = 0; i < n_token; i++) {
+            printf("    ");
+            // for (int j = 0; j < dim; j++) {
+            for (int j = 0; j < 16; j++) {
+                printf("0x%04x ", ((uint16_t *)(hbm_addr(in_token_offset)))[j + i * dim]);
+            }
+            printf("\n");
+        }
+        printf("gate_weights:\n");
+        for (int i = 0; i < n_token; i++) {
+            printf("    ");
+            // for (int j = 0; j < dim; j++) {
+            for (int j = 0; j < 16; j++) {
+                printf("0x%04x ", ((uint16_t *)(hbm_addr(gate_weights_offset)))[j + i * dim]);
+            }
+            printf("\n");
+        }
+        printf("actual_out:\n");
+        for (int i = 0; i < n_token; i++) {
+            printf("    ");
+            // for (int j = 0; j < dim; j++) {
+            for (int j = 0; j < 16; j++) {
+                printf("0x%04x ", ((uint16_t *)(hbm_addr(actual_out_offset)))[j + i * dim]);
+            }
+            printf("\n");
+        }
+        // printf("0x%04x\n", ((uint16_t *)(hbm_addr(in_token_addr)))[0]);
         // printf("0x%04x\n", ((uint16_t *)(hbm_addr(gate_weights_addr)))[0]);
         // printf("0x%04x\n", ((uint16_t *)(hbm_addr(expert_w1_weights_addr)))[0]);
         // printf("0x%04x\n", ((uint16_t *)(hbm_addr(expert_w1_bias_addr)))[0]);
@@ -81,7 +123,7 @@ int main(){
         printf("[Start MoE Computation]\n");
         flex_timer_start();
     }
-    // compute_moe(in_token_addr, n_token, dim, inter_dim, n_routed_experts, n_shared_experts, n_activated_experts, gate_weights_addr, expert_w1_weights_addr, expert_w1_bias_addr, expert_w2_weights_addr, expert_w2_bias_addr, expert_w3_weights_addr, expert_w3_bias_addr, actual_out_addr);
+    compute_moe(in_token_offset, n_token, dim, inter_dim, n_routed_experts, n_shared_experts, n_activated_experts, gate_weights_offset, expert_w1_weights_offset, expert_w1_bias_offset, expert_w2_weights_offset, expert_w2_bias_offset, expert_w3_weights_offset, expert_w3_bias_offset, actual_out_offset);
     flex_global_barrier_xy();
     if (flex_get_core_id() == 0 && flex_get_cluster_id() == 0) {
         flex_timer_end();
@@ -95,7 +137,7 @@ int main(){
             printf("    ");
             // for (int j = 0; j < dim; j++) {
             for (int j = 0; j < 8; j++) {
-                printf("0x%04x ", ((uint16_t *)(hbm_addr(actual_out_addr)))[j + i * dim]);
+                printf("0x%04x ", ((uint16_t *)(hbm_addr(actual_out_offset)))[j + i * dim]);
             }
             printf("\n");
         }
