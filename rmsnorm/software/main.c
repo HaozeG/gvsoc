ad3fc5b8cee5ad0e443f50ca651b578d7e88eb3f
Haoze <haozegao0602@gmail.com>
2025-07-24 19:57:48 +0200

    modify data division strategy; better print

--- rmsnorm/software/main.c
+++ rmsnorm/software/main.c
@@ -17,25 +17,25 @@ int main(){
 
     uint32_t eoc_val = 0;
     // TODO: read parameters from command line or configuration file
-    uint32_t n_token = 4;
+    uint32_t n_token = 5;
     // uint16_t dim = 1024;
-    uint16_t dim = 20; // feature dimension
-    fp16 dim_fp16 = float_to_fp16((float)dim);
+    uint16_t dim = 140; // feature dimension
     uint32_t input_offset = 0x00000000; // TCDM offset for input data
 
-    uint32_t tile_size_dim = (dim - 1) / ARCH_NUM_CLUSTER + 1; // ideal tile size along dim
-    uint32_t actual_tile_size_dim = max(min(tile_size_dim, dim - flex_get_cluster_id() * tile_size_dim), 0);
+    // TODO: consider better division strategy
+    uint32_t tile_size_dim = dim / ARCH_NUM_CLUSTER; // ideal tile size along dim
+    uint32_t actual_tile_size_dim = (flex_get_cluster_id() < (dim % ARCH_NUM_CLUSTER)) ? (tile_size_dim + 1) : tile_size_dim; // actual tile size along dim
     // assign initial values to input data
     if (flex_is_dm_core()) {
-        uint16_t * local_in_ptr = (uint16_t *)local(input_offset);
+        fp16 * local_in_ptr = (fp16 *)local(input_offset);
         #ifdef PRINT_DEBUG
-        if (flex_get_cluster_id() == 0) {
+        if (flex_get_cluster_id() == PRINT_DEBUG_CLUSTER_ID) {
             printf("[Input Data Initialization]\n");
         }
         #endif
         for (int i = 0; i < n_token; i++) {
             #ifdef PRINT_DEBUG
-            if (flex_get_cluster_id() == 0) {
+            if (flex_get_cluster_id() == PRINT_DEBUG_CLUSTER_ID) {
                 printf("    ");
             }
             #endif
@@ -43,13 +43,13 @@ int main(){
                 // TODO: initial values to be determined
                 local_in_ptr[j + i * actual_tile_size_dim] = 0x3c00;
                 #ifdef PRINT_DEBUG
-                if (flex_get_cluster_id() == 0) {
-                    printf("0x%04x ", local_in_ptr[j + i * actual_tile_size_dim]);
+                if (flex_get_cluster_id() == PRINT_DEBUG_CLUSTER_ID) {
+                    printf("%f ", fp16_to_float(local_in_ptr[j + i * actual_tile_size_dim]));
                 }
                 #endif
             }
             #ifdef PRINT_DEBUG
-            if (flex_get_cluster_id() == 0) {
+            if (flex_get_cluster_id() == PRINT_DEBUG_CLUSTER_ID) {
                 printf("\n");
             }
             #endif
@@ -64,28 +64,37 @@ int main(){
     flex_global_barrier_xy();
 
     // call compute function
-    compute_rmsnorm(input_offset, n_token, dim_fp16, dim);
+    compute_rmsnorm(input_offset, n_token, dim);
 
     flex_global_barrier_xy();
     if (flex_get_core_id() == 0 && flex_get_cluster_id() == 0) {
         flex_timer_end();
     }
-    
-#ifdef PRINT_DEBUG
-    // get the output data
-    if (flex_is_first_core() && (flex_get_cluster_id() == 0))
-    {
-        printf("[Check Results]\n");
+
+    // print results
+    #ifdef PRINT_DEBUG
+    if (flex_is_dm_core()) {
+        fp16 * local_in_ptr = (fp16 *)local(input_offset);
+        if (flex_get_cluster_id() == PRINT_DEBUG_CLUSTER_ID) {
+            printf("[Check Output]\n");
+        }
+
         for (int i = 0; i < n_token; i++) {
-            printf("    ");
-            // for (int j = 0; j < actual_tile_size_dim; j++) {
-            for (int j = 0; j < min(32, actual_tile_size_dim); j++) {
-                printf("0x%04x ", ((uint16_t *)(input_offset))[j + i * actual_tile_size_dim]);
+            if (flex_get_cluster_id() == PRINT_DEBUG_CLUSTER_ID) {
+                printf("    ");
             }
-            printf("\n");
+            for (int j = 0; j < actual_tile_size_dim; j++) {
+                if (flex_get_cluster_id() == PRINT_DEBUG_CLUSTER_ID) {
+                    printf("%f ", fp16_to_float(local_in_ptr[j + i * actual_tile_size_dim]));
+                }
+            }
+            if (flex_get_cluster_id() == PRINT_DEBUG_CLUSTER_ID) {
+                printf("\n");
+            }
         }
     }
-#endif
+    #endif
+    
     flex_global_barrier_xy();
     flex_eoc(eoc_val);
     return 0;
